a2feb79d1ab5ac7e49b554ce22f114ff43bb4329
wuchuanchuan <superwucc@gmail.com>
2020-02-19 10:48:35 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,156 @@
+(function () {
+
+    // 是否显示
+    var showCommonTip = function () {
+        let ua = navigator.userAgent,
+            isAndroid = /android|adr|linux/gi.test(ua),
+            isIOS = /iphone|ipod|ipad/gi.test(ua) && !isAndroid,
+            isBlackBerry = /BlackBerry/i.test(ua),
+            isWindowPhone = /IEMobile/i.test(ua),
+            isMobile = isAndroid || isIOS || isBlackBerry || isWindowPhone;
+
+        // 微信、非一卡通商城
+        let isYikatong = /ecton/gi.test(ua); // 判断是否在一卡通 APP 打开
+        if (/MicroMessenger/i.test(ua) || (isMobile && !isYikatong)) {
+            $('#m_common_tip').css('display', 'flex');
+        }
+    };
+
+    /**
+     * 判断手机版本
+     */
+    var detectVersion = function () {
+        let isAndroid, isIOS, isIOS9, version,
+            u = navigator.userAgent,
+            ua = u.toLowerCase();
+
+        if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) { //android终端或者uc浏览器
+            //Android系统
+            isAndroid = true
+        }
+
+        if (ua.indexOf("like mac os x") > 0) {
+            //ios
+            var regStr_saf = /os [\d._]*/gi;
+            var verinfo = ua.match(regStr_saf);
+            version = (verinfo + "").replace(/[^0-9|_.]/ig, "").replace(/_/ig, ".");
+        }
+        var version_str = version + "";
+        if (version_str != "undefined" && version_str.length > 0) {
+            version = parseInt(version)
+            if (version >= 8) {
+                // ios9以上
+                isIOS9 = true
+            } else {
+                isIOS = true
+            }
+        }
+        return {
+            isAndroid,
+            isIOS,
+            isIOS9
+        }
+    }
+
+    /**
+     * 判断手机上是否安装了app，如果安装直接打开url，如果没安装，执行callback
+     */
+    var openApp = function (url, callback) {
+        let {
+            isAndroid,
+            isIOS,
+            isIOS9
+        } = detectVersion()
+        if (isAndroid || isIOS) {
+            var timeout, t = 4000,
+                hasApp = true;
+            var openScript = setTimeout(function () {
+                if (!hasApp) {
+                    callback && callback()
+                }
+                document.body.removeChild(ifr);
+            }, 5000)
+
+            var t1 = Date.now();
+            var ifr = document.createElement("iframe");
+            ifr.setAttribute('src', url);
+            ifr.setAttribute('style', 'display:none');
+            document.body.appendChild(ifr);
+
+            timeout = setTimeout(function () {
+                var t2 = Date.now();
+                if (t2 - t1 < t + 100) {
+                    hasApp = false;
+                }
+            }, t);
+        }
+
+        if (isIOS9) {
+            location.href = url;
+            setTimeout(function () {
+                callback && callback()
+            }, 250);
+            setTimeout(function () {
+                location.reload();
+            }, 1000);
+        }
+    }
+
+    $(document).ready(function () {
+        // 是否显示
+        showCommonTip();
+        // 点击打开
+        $('#m_common_tip .content-right').on('click', function () {
+            let ua = navigator.userAgent;
+            let isWeiXin = /MicroMessenger/i.test(ua);
+
+            // 判断是否登录，isUserLogin 这个参数是 详情页面 goods.detail.new.js 里面的变量
+            // if (!isUserLogin) {
+            //     if (isWeiXin) {
+            //         let k = getCookie("isTourist"); // getCookie 是 goods.detail.new.js 里面的方法
+            //         window.location.href = GLOBAL_INFO.COMMON_URI + "/register.html?k=" + k + "&url_jump=" + window.location.href;
+            //     } else {
+            //         window.location.href = GLOBAL_INFO.COMMON_URI + "/login.html?url_jump=" + window.location.href;
+            //     }
+            // }
+
+            if (isWeiXin) {
+                // 调用框架的弹窗提示在原生浏览器打开
+                console.log('调用框架的弹窗提示在原生浏览器打开');
+                console.log($('#m_common_tip_mask'));
+                $('#m_common_tip_mask').css('display', 'block');
+            } else {
+                // 调用 APP 的方法打开 App，需要咨询 APP 开发人员
+                console.log('调用 APP 的方法打开 App');
+                let currentUrl = window.location.href;
+                let openUrl = 'ecton://com.ecton.app/openWeb?url=' + currentUrl;
+                let androidDownloadUrl = 'https://android.myapp.com/myapp/detail.htm?apkName=com.ecton.app&ADTAG=mobile';
+                let iosDownLoadUrl = 'https://apps.apple.com/cn/app/%E5%B1%B1%E4%B8%9C%E4%B8%80%E5%8D%A1%E9%80%9Ae%E7%94%9F%E6%B4%BB/id1380052212';
+                // window.location.href = 'ecton://com.ecton.app/openWeb?url=' + currentUrl;
+                openApp(openUrl, function () {
+                    let {
+                        isAndroid,
+                        isIOS,
+                        isIOS9
+                    } = detectVersion();
+                    if (isAndroid) {
+                        console.log('打开安卓应用市场');
+                        window.location.href = androidDownloadUrl;
+                    } else {
+                        console.log('打开App Store');
+                        window.location.href = iosDownLoadUrl;
+                    }
+                });
+            }
+        })
+        // 点击取消
+        $('#m_common_tip .tip-close-btn').on('click', function () {
+            $('#m_common_tip').css('display', 'none');
+        })
+
+        // 点击遮罩关闭提示
+        $('#m_common_tip_mask').on('click', function () {
+            $('#m_common_tip_mask').css('display', 'none');
+        })
+    });
+})()
